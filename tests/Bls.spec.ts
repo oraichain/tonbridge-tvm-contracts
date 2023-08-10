616d5b28a34a8aa65b9908da64edcede45784c40
Anton Varlamov <anton.varlamov@rsquad.io>
2023-08-10 18:45:01 +0300

    check noble curve

--- tests/Bls.spec.ts
+++ tests/Bls.spec.ts
@@ -1,4 +1,4 @@
-import {PublicKey, Signature, fastAggregateVerify} from '@chainsafe/blst';
+import {PublicKey, SecretKey, Signature, aggregatePubkeys, fastAggregateVerify, verify} from '@chainsafe/blst';
 import {bls12_381 as bls} from '@noble/curves/bls12-381';
 import {compile} from '@ton-community/blueprint';
 import {Blockchain, SandboxContract} from '@ton-community/sandbox';
@@ -52,39 +52,58 @@ describe('BlsContract', () => {
         const calcHashRes = await readerContract.sendVerify(increaser.getSender(), {
             value: toNano('0.5'),
             receipt: beginCell()
-            .storeBuffer(bytes('0xa491d1b0ecd9bb917989f0e74f0dea0422eac4a873e5e2644f368dffb9a6e20fd6e10c1b77654d067c0618f6e5a7f79a'))
-            .storeRef(
-              beginCell()
-              .storeBuffer(bytes('0xb301803f8b5ac4a1133581fc676dfedc60d891dd5fa99028805e5ea5b08d3491af75d0707adab3b70c6a6a580217bf81'))
-              .storeRef(
-                beginCell()
-                .storeBuffer(bytes('0xb53d21a4cfd562c469cc81514d4ce5a6b577d8403d32a394dc265dd190b47fa9f829fdd7963afdf972e5e77854051f6f'))
-
-                .storeRef(
-                  beginCell()
-                  .storeBuffer(bytes('0x3'))
-                  .storeRef(
-                    beginCell()
-                    .storeBuffer(bytes('0x5656565656565656565656565656565656565656565656565656565656565656'))
-                    .storeRef(
-                      beginCell()
-                      .storeBuffer(bytes('0x912c3615f69575407db9392eb21fee18fff797eeb2fbe1816366ca2a08ae574d8824dbfafb4c9eaa1cf61b63c6f9b69911f269b664c42947dd1b53ef1081926c1e82bb2a465f927124b08391a5249036146d6f3f1e17ff5f162f779746d830d1'))
-                      .endCell()
+                .storeBuffer(
+                    bytes(
+                        '0xa491d1b0ecd9bb917989f0e74f0dea0422eac4a873e5e2644f368dffb9a6e20fd6e10c1b77654d067c0618f6e5a7f79a'
                     )
-                    .endCell()
-                  )
-                  .endCell()
                 )
+                .storeRef(
+                    beginCell()
+                        .storeBuffer(
+                            bytes(
+                                '0xb301803f8b5ac4a1133581fc676dfedc60d891dd5fa99028805e5ea5b08d3491af75d0707adab3b70c6a6a580217bf81'
+                            )
+                        )
+                        .storeRef(
+                            beginCell()
+                                .storeBuffer(
+                                    bytes(
+                                        '0xb53d21a4cfd562c469cc81514d4ce5a6b577d8403d32a394dc265dd190b47fa9f829fdd7963afdf972e5e77854051f6f'
+                                    )
+                                )
 
-                .endCell()
-              )
-              .endCell()
-            )
-            .endCell()
-            ,
+                                .storeRef(
+                                    beginCell()
+                                        .storeBuffer(bytes('0x3'))
+                                        .storeRef(
+                                            beginCell()
+                                                .storeBuffer(
+                                                    bytes(
+                                                        '0x5656565656565656565656565656565656565656565656565656565656565656'
+                                                    )
+                                                )
+                                                .storeRef(
+                                                    beginCell()
+                                                        .storeBuffer(
+                                                            bytes(
+                                                                '0x912c3615f69575407db9392eb21fee18fff797eeb2fbe1816366ca2a08ae574d8824dbfafb4c9eaa1cf61b63c6f9b69911f269b664c42947dd1b53ef1081926c1e82bb2a465f927124b08391a5249036146d6f3f1e17ff5f162f779746d830d1'
+                                                            )
+                                                        )
+                                                        .endCell()
+                                                )
+                                                .endCell()
+                                        )
+                                        .endCell()
+                                )
+
+                                .endCell()
+                        )
+                        .endCell()
+                )
+                .endCell(),
         });
 
-        console.log(calcHashRes.transactions.map(t => t.vmLogs));
+        // console.log(calcHashRes.transactions.map(t => t.vmLogs));
 
         await expect(calcHashRes.transactions).toHaveTransaction({
             from: increaser.address,
@@ -101,33 +120,116 @@ describe('BlsContract', () => {
         // console.log(r.hash().toString('hex'))
 
         const res = await fastAggregateVerify(
-          bytes('0x5656565656565656565656565656565656565656565656565656565656565656'),
-          [
-            PublicKey.fromBytes(bytes('0xa491d1b0ecd9bb917989f0e74f0dea0422eac4a873e5e2644f368dffb9a6e20fd6e10c1b77654d067c0618f6e5a7f79a')),
-            PublicKey.fromBytes(bytes('0xb301803f8b5ac4a1133581fc676dfedc60d891dd5fa99028805e5ea5b08d3491af75d0707adab3b70c6a6a580217bf81')),
-            PublicKey.fromBytes(bytes('0xb53d21a4cfd562c469cc81514d4ce5a6b577d8403d32a394dc265dd190b47fa9f829fdd7963afdf972e5e77854051f6f')),
-          ],
-          Signature.fromBytes(bytes('0x912c3615f69575407db9392eb21fee18fff797eeb2fbe1816366ca2a08ae574d8824dbfafb4c9eaa1cf61b63c6f9b69911f269b664c42947dd1b53ef1081926c1e82bb2a465f927124b08391a5249036146d6f3f1e17ff5f162f779746d830d1')) ,
+            bytes('0x5656565656565656565656565656565656565656565656565656565656565656'),
+            [
+                PublicKey.fromBytes(
+                    bytes(
+                        '0xa491d1b0ecd9bb917989f0e74f0dea0422eac4a873e5e2644f368dffb9a6e20fd6e10c1b77654d067c0618f6e5a7f79a'
+                    )
+                ),
+                PublicKey.fromBytes(
+                    bytes(
+                        '0xb301803f8b5ac4a1133581fc676dfedc60d891dd5fa99028805e5ea5b08d3491af75d0707adab3b70c6a6a580217bf81'
+                    )
+                ),
+                PublicKey.fromBytes(
+                    bytes(
+                        '0xb53d21a4cfd562c469cc81514d4ce5a6b577d8403d32a394dc265dd190b47fa9f829fdd7963afdf972e5e77854051f6f'
+                    )
+                ),
+            ],
+            Signature.fromBytes(
+                bytes(
+                    '0x912c3615f69575407db9392eb21fee18fff797eeb2fbe1816366ca2a08ae574d8824dbfafb4c9eaa1cf61b63c6f9b69911f269b664c42947dd1b53ef1081926c1e82bb2a465f927124b08391a5249036146d6f3f1e17ff5f162f779746d830d1'
+                )
+            )
         );
 
-        console.log(res);
+        console.log({ isValid: res });
     });
 
     it('should emit correct hash 2', async () => {
-      const privateKeys = [
-        '18f020b98eb798752a50ed0563b079c125b0db5dd0b1060d1c1b47d4a193e1e4',
-        'ed69a8c50cf8c9836be3b67c7eeff416612d45ba39a5c099d48fa668bf558c9c',
-        '16ae669f3be7a2121e17d0c68c05a8f3d6bef21ec0f2315f1d7aec12484e4cf5',
-      ];
-      const message = '64726e3da8';
-      const publicKeys = privateKeys.map(bls.getPublicKey);
+        const privateKeys = [
+            '18f020b98eb798752a50ed0563b079c125b0db5dd0b1060d1c1b47d4a193e1e4',
+            'ed69a8c50cf8c9836be3b67c7eeff416612d45ba39a5c099d48fa668bf558c9c',
+            '16ae669f3be7a2121e17d0c68c05a8f3d6bef21ec0f2315f1d7aec12484e4cf5',
+        ];
+        const message = '64726e3da8';
+        const publicKeys = privateKeys.map(bls.getPublicKey);
 
-      const signatures = privateKeys.map((p) => bls.sign(message, p));
+        const signatures = privateKeys.map((p) => bls.sign(message, p));
 
-      const aggPubKey = bls.aggregatePublicKeys(publicKeys);
-      const aggSignature = bls.aggregateSignatures(signatures);
-      const isValid = bls.verify(aggSignature, message, aggPubKey);
-      console.log({ signatures: signatures, aggSignature, isValid });
-    })
+        const aggPubKey = bls.aggregatePublicKeys(publicKeys);
+        const aggSignature = bls.aggregateSignatures(signatures);
+        const isValid = bls.verify(aggSignature, message, aggPubKey);
+
+        console.log({ isValid });
+        // publs, N, msg, sig
+        const dataForBls = [
+            ...publicKeys.map((p) => Buffer.from(p)),
+            bytes('0x3'),
+            bytes('0x' + message),
+            Buffer.from(aggSignature),
+        ]
+            .map((item) => {
+                // console.log(item);
+                return beginCell().storeBuffer(item);
+            })
+            .reverse()
+            .reduce((acc, memo) => {
+                if (!acc) {
+                    return memo.endCell();
+                }
+                return memo.storeRef(acc).endCell();
+            }, null as any as Cell);
+
+        const increaser = await blockchain.treasury('increaser');
+        const calcHashRes = await readerContract.sendVerify(increaser.getSender(), {
+            value: toNano('0.5'),
+            receipt: dataForBls,
+        });
+
+        // console.log(calcHashRes.transactions.map(t => t.vmLogs));
+
+        await expect(calcHashRes.transactions).toHaveTransaction({
+            from: increaser.address,
+            to: readerContract.address,
+            success: true,
+        });
+
+        // expect(calcHashRes.externals.length).toEqual(1);
+
+        const externalOutBodySlice = calcHashRes.externals[0].body.asSlice();
+        console.log(externalOutBodySlice);
+
+        const res0 = verify(
+            bytes('0x' + message),
+            aggregatePubkeys([...publicKeys.map((p) => PublicKey.fromBytes(p))]),
+            Signature.fromBytes(aggSignature)
+        );
+
+        console.log({res0});
+
+        const res = await fastAggregateVerify(
+            bytes('0x' + message),
+            [
+                // PublicKey.fromBytes(bytes(aggPubKey))
+                ...publicKeys.map((p) => PublicKey.fromBytes(Buffer.from(p))),
+            ],
+            Signature.fromBytes(Buffer.from(aggSignature))
+        );
+
+        console.log({ isValid: res });
+
+        const privKey =  '18f020b98eb798752a50ed0563b079c125b0db5dd0b1060d1c1b47d4a193e1e4';
+        const pubkey = bls.getPublicKey(privKey);
+        const sk = SecretKey.fromBytes(bytes('0x' + privKey));
+        const pub = sk.toPublicKey();
+
+        const signature = bls.sign(message, privKey);
+        const sig = sk.sign(Buffer.from(message, 'hex'));
+        console.log(Buffer.from(sk.toBytes()).toString('hex'))
+        console.log(pubkey, pub.toBytes());
+        console.log(Buffer.from(sig.toBytes()).toString('hex'), Buffer.from(signature).toString('hex'));
+    });
 });
-
